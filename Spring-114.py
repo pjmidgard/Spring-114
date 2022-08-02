ee80ef740bec8eeab16c5bb7a6d74ba8bf69b9de
Java Software <pj.midgard@yahoo.com>
2022-08-02 19:15:40 +0100

    Update Spring-114.py

--- Spring-114.py
+++ Spring-114.py
@@ -460,6 +460,12 @@ class compression:
                     else:
                             print('Path is not exists!')
                             raise SystemExit
+                            
+                    x=0
+                    x1=0
+                    x2=0
+                    x = time()
+                    
                     Portal=2
                     namea="file.W"
                     namem=""
@@ -519,10 +525,7 @@ class compression:
 
                     block=1
 
-                    x=0
-                    x1=0
-                    x2=0
-                    x = time()
+                    
 
                     with open(nameas, "w") as f4:
                             f4.write(s)
