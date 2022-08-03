aa9bc7cbb6b52e09dc28ba439f7795fe2138c3d9
Java Software <pj.midgard@yahoo.com>
2022-08-03 10:30:33 +0100

    Update Spring-114.py

--- Spring-114.py
+++ Spring-114.py
@@ -222,8 +222,6 @@ class compression:
                                                     size_compress=63
                                                     end=blocks
                                                     
-                                                    find_matches1_number1=0
-                                                
                                                     if predict3==1 or predict3==4:
                                                        predict=predict+1
                                                     elif predict3==2 or predict3==3:
